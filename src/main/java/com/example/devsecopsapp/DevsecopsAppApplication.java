7335222019a4c33b693922a17d39193f8027c5a0
Harshita Chabaria <chabariaharshita24@gmail.com>
2025-09-25 15:48:10 +0530

    Final CI/CD workflow ready for submission

---
+++ src/main/java/com/example/devsecopsapp/DevsecopsAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.devsecopsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsecopsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevsecopsAppApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/devsecopsapp/HealthController.java
@@ -0,0 +1,14 @@
+
+package com.example.devsecopsapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "App is running OK ✅";
+    }
+}
